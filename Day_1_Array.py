17fbbca19aa5efad5f9308e6f530556767819834
Palak Gupta <pg3320310@gmail.com>
2026-03-01 01:31:58 +0530

    Added Day 1 Array program

---
+++ Day_1_Array.py
@@ -0,0 +1,17 @@
+# numbers = [1, 2, 3, 4]
+
+# total = 0
+
+# for num in numbers:
+#     total = total + num
+
+# print("Sum is:", total)
+
+numbers = [1,3,4,5,6,9]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num>largest:
+        largest = num
+print("Largest number is :",largest)
