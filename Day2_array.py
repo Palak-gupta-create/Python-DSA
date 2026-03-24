27d8e3d8d478df102355036db15b5e826161be51
Palak-gupta-create <pg3320310@gmail.com>
2026-03-25 00:01:52 +0530

    Update Day2_array.py

--- Day2_array.py
+++ Day2_array.py
@@ -1,21 +1,13 @@
-nums = [2,7,11,15]
-target = 9
-
-for i in range(len(nums)):
-    for j in range(i+1, len(nums)):
-        if nums[i] + nums[j] == target:
-            print(i, j)
-
 # Optimized Solution
 
-# nums = [2,7,11,15]
-# target = 9
-# num_dict = {}
+nums = [2,7,11,15]
+target = 9
+num_dict = {}
 
-# for i in range(len(nums)):
-#     complement = target - nums[i]
+for i in range(len(nums)):
+    complement = target - nums[i]
     
-#     if complement in num_dict:
-#         print(num_dict[complement], i)
+    if complement in num_dict:
+        print(num_dict[complement], i)
     
-#     num_dict[nums[i]] = i
+    num_dict[nums[i]] = i
